8b2685264d771a79c529a250918e75621eeb8d62
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2022-12-19 21:40:03 +0100

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,36 @@
 
+const loginFormEl = document.querySelector('.login-form');
+const buttonSubmitEl = document.querySelector('button');
+const inputEmailEl = document.querySelector("input[name='email']");
+const inputPasswordEl = document.querySelector("input[name='password']");
+
+// const arrLoginFormElements = {};
+
+buttonSubmitEl.addEventListener('click', onLoginForm);
+
+function onLoginForm() {
+    loginFormEl.addEventListener('submit', onStopUpdate);
+};
+
+
+function onStopUpdate(evt) {
+    evt.preventDefault();
+
+    if(!(inputEmailEl.value) && !(inputPasswordEl.value)) {
+
+        alert('Form must be completed!');
+
+    } else {
+
+        const loginFormElements = evt.currentTarget;
+        const arrLoginFormElements = {
+            email: loginFormElements.email.value,
+            password: loginFormElements.email.value,
+        };
+        console.log(arrLoginFormElements);
+
+        loginFormEl.reset();
+    };
+
+};
+
